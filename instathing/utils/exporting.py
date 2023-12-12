18686894f29379ec4578b7fa9fe2afa7eaca84f9
Daniel Rocha <danielsantos852@gmail.com>
2023-12-12 15:02:40 -0300

    Update exporting.py
    Minor adjustments.

--- instathing/utils/exporting.py
+++ instathing/utils/exporting.py
@@ -5,13 +5,16 @@ import pyarrow.parquet as pq
 
 
 # Export Pandas DataFrame to Parquet file function
-def dataframe_to_parquet(df:pd.core.frame.DataFrame, path:str) -> None:
+def dataframe_to_parquet(
+    df:pd.core.frame.DataFrame,
+    path_to_file:str
+) -> None:
     
     # Create pyarrow table from pandas dataframe
     table = pa.Table.from_pandas(df)
     
     # Create parquet file
-    pqwriter = pq.ParquetWriter(where=f'{path}.parquet', schema=table.schema)
+    pqwriter = pq.ParquetWriter(where=f'{path_to_file}', schema=table.schema)
     
     # Add table to parquet file
     pqwriter.write_table(table=table)
