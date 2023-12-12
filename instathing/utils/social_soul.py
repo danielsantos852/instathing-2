d7c864ab12a28de7dfab4b70e6bb66558701eaba
Daniel Rocha <danielsantos852@gmail.com>
2023-12-12 16:54:21 -0300

    Update social_soul.py
    Renamed functions:
    - From refine_dataframe() to dataframe_refine()
    - From add_ig_stories_to_df() to dataframe_generate_ig_story_images()
    
    Minor adjustments.

--- instathing/utils/social_soul.py
+++ instathing/utils/social_soul.py
@@ -1,50 +1,50 @@
-# External imports
+# Imports
 import pandas as pd
 import numpy as np
-
-# Internal imports
-from .ig_image_processing import generate_ig_story_image
+from .image_manipulation import create_ig_story_image_from_offer
 
 
-# Refine Dataframe (Social Soul) function
-def refine_dataframe(df:pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+# Refine Dataframe function
+def dataframe_refine(df:pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     
-    # STEP 1: Select columns of interest
-    # Set columns of interest
-    columns_of_interest = [
-        'offerName',
-        #'sellerId',
-        #'sellerThumbnail',
-        'offerLink',
-        'offerThumbnail',
-        'priceFrom',
-        #'sellerName',
-        'priceTo',
-        #'sku',
-        #'categoryName',
-        #'categoryId',
+    # Select columns of interest and clip dataframe
+    # (Comment/uncomment to clip/keep a column)
+    df = df.loc[
+        :,[
+            'offerName',            # Offer description
+            #'sellerId',
+            #'sellerThumbnail',
+            'offerLink',            # URL to offer
+            'offerThumbnail',       # Photo of product in white background (most of the time)
+            'priceFrom',            # Price without discount (price before)
+            #'sellerName',
+            'priceTo',              # Price with discount (price after)
+            #'sku',
+            #'categoryName',
+            #'categoryId',
+        ]
     ]
-    # Select columns of interest only
-    df = df.loc[:,columns_of_interest]
-    
-    
-    # STEP 2: Cast prices from STR to FLOAT64
+
+    # Cast prices from STR to FLOAT64
     df['priceFrom'] = df['priceFrom'].astype('float64')
     df['priceTo'] = df['priceTo'].astype('float64')
-    
-    
-    # STEP 3: Clean absent values
-    # Replace NaN (Not a Number) with None
+
+    # Clean empty cells (replace NaN with None)
     df = df.replace(np.nan, None)
-    
-    
-    # Return refined dataframe
+
+    # Return dataframe
     return df
 
 
-# Add Instagram Stories To Pandas Dataframe function
-def add_ig_stories_to_df(df:pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+# Dataframe Generate IG Stories function
+def dataframe_generate_ig_story_images(
+    df:pd.core.frame.DataFrame,
+    output_folder='./temp/images/'
+) -> pd.core.frame.DataFrame:
     
+    # Parameters
+    path_to_story_template_image = './resources/images/story_template_720x1280_final.png'
+
     # Create empty list of IG Stories images
     ig_stories_images = []
     
@@ -53,12 +53,13 @@ def add_ig_stories_to_df(df:pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
         
         # Generate IG Stories image
         ig_stories_images.append(
-            generate_ig_story_image(
+            create_ig_story_image_from_offer(
+                base_image=path_to_story_template_image,
                 offer_thumbnail= df.loc[i, 'offerThumbnail'],
                 offer_name= df.loc[i, 'offerName'],
                 offer_price_from= df.loc[i, 'priceFrom'],
                 offer_price_to= df.loc[i, 'priceTo'],
-                file_path= f'./temp/ig_stories_images/offer_{i}.png',
+                output_path= f'{output_folder}offer{str(i).zfill(3)}.png' # e.g.: 'offer001.png',
             )
         )
     
