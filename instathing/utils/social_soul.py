ee3a7fe9cf44bb394e21e3b5467b31a4da9b2231
Daniel Rocha <danielsantos852@gmail.com>
2023-12-18 17:35:00 -0300

    Update social_soul.py

--- instathing/utils/social_soul.py
+++ instathing/utils/social_soul.py
@@ -1,27 +1,31 @@
 # Imports
+from textwrap import wrap
+
 import numpy as np
 from pandas.core.frame import DataFrame
+from PIL import Image
+import requests
 
 import config as cfg
-from .image_manipulation import create_ig_story_image_from_offer
+from .image_manipulation import add_text_to_image
 
 
 # Global Variables
 COLUMNS_OF_INTEREST = cfg.COLUMNS_OF_INTEREST_LIST
 PATH_TO_IMG_OUTPUT_FOLDER = cfg.DEFAULT_PATH_TO_TMP_IMAGE_FOLDER
-PATH_TO_IG_STORY_TEMPLATE_IMG = cfg.DEFAULT_PATH_TO_STORY_TEMPLATE
+PATH_TO_IG_STORY_TEMPLATE = cfg.PATH_TO_DEFAULT_STORY_TEMPLATE_IMG
 
 
 # Refine Dataframe function
-def dataframe_refine(df:DataFrame) -> DataFrame:
+def refine_df(df:DataFrame) -> DataFrame:
     """
-    Refines a pandas dataframe with raw offers data from Social Soul XML file
+    Refines a pandas dataframe with raw offer data from Social Soul.
 
     Parameters:
-        df (DataFrame): a pandas dataframe
-    
+        df (DataFrame): a pandas dataframe.
+
     Returns:
-        df (DataFrame): a pandas dataframe
+        df (DataFrame): same dataframe, refined.
     """
     # Clip dataframe using columns of interest (configurable in config.py)
     df = df.loc[:, COLUMNS_OF_INTEREST]
@@ -37,42 +41,42 @@ def dataframe_refine(df:DataFrame) -> DataFrame:
     return df
 
 
-# Generate IG Story Images from Dataframe function
-def gen_ig_story_imgs_from_df(
+# Generate IG Story Images for Dataframe function
+def gen_ig_story_imgs_for_df(
     df:DataFrame = None,
     output_folder = PATH_TO_IMG_OUTPUT_FOLDER
 ) -> DataFrame:
     """ 
-    Generates offer images for IG story posting using data from input df, 
-    saves images in output_folder. Appends column of image paths to input df, 
-    also removes columns no longer needed.
+    Generates offer images for IG story posting using offer data from df, 
+    saves images in output_folder, appends column of image paths to df, also 
+    removes no longer necessary columns from df.
     
     Parameters:
-        df (DataFrame): a pandas dataframe containing offer data
-        output_folder (str): path to folder were images will be stored
+        df (DataFrame): a pandas dataframe containing refined offer data;
+        output_folder (str): path to folder where images will be stored.
 
     Returns:
-        df (DataFrame): same pandas dataframe with less columns but a new one
+        df (DataFrame): input dataframe with less columns but a new one.
     """
-    # Create empty list of IG Stories images
+    # Create empty list of IG story images
     ig_story_images = []
     
     # For each offer in offers dataframe:
     for i in range(len(df)):
         
-        # Generate IG Stories image
+        # Generate IG story image
         ig_story_images.append(
-            create_ig_story_image_from_offer(
-                base_image=PATH_TO_IG_STORY_TEMPLATE_IMG,
+            gen_offer_ig_story_img(
+                output_file= f'{output_folder}offer{str(i).zfill(3)}.png', # e.g.: "offer001.png",
+                base_image=PATH_TO_IG_STORY_TEMPLATE,
                 offer_thumbnail= df.loc[i, 'offerThumbnail'],
                 offer_name= df.loc[i, 'offerName'],
                 offer_price_from= df.loc[i, 'priceFrom'],
                 offer_price_to= df.loc[i, 'priceTo'],
-                output_path= f'{output_folder}offer{str(i).zfill(3)}.png' # e.g.: 'offer001.png',
             )
         )
     
-    # Add IG Stories images list as column in dataframe
+    # Add IG story images list as column in dataframe
     df['igStoryImage'] = ig_story_images
     
     # Drop columns no longer needed
@@ -84,3 +88,77 @@ def gen_ig_story_imgs_from_df(
     
     # Return dataframe
     return df
+
+
+# Generate Instagram Story Image from Offer function
+def gen_offer_ig_story_img(
+    output_file:str = './tmp/img/test.png',
+    base_image:str = './rsc/img/tmpl_story_720x1280_blue.png',
+    offer_thumbnail:str = './rsc/img/fake_offer_thumbnail_640x640.png',
+    offer_name:str = 'Awesome shoes!',
+    offer_price_from:str = '9999.99',
+    offer_price_to:str = '0000.00',
+) -> str:
+    """
+    Generates a 720x1280 offer image fit for an IG Stories post.
+    
+    Parameters:
+        output_file (str): path to output image file
+        base_image (str): path to 720x1280 template image
+        offer_thumbnail (str): url to product thumbnail
+        offer_name (str): product description
+        offer_price_from (str): product price before discount
+        offer_price_to (str): product price after discount
+
+    Returns:
+        output_file (str): same as input
+    """
+    # Create new IG story image from base image
+    im_story = Image.open(fp=base_image)
+    
+    # Download and paste offer thumbnail to IG story image
+    im_thumbnail = Image.open(requests.get(offer_thumbnail, stream=True).raw)
+    im_thumbnail = im_thumbnail.resize(size=(640,640))
+    im_story.paste(im=im_thumbnail,box=(40, 130))
+    
+    # Clip offer name if max length exceeded
+    max_name_length = 50
+    if len(offer_name) > max_name_length:
+        if offer_name[max_name_length-4]==' ':
+            offer_name = f'{offer_name[:max_name_length-4]}...'
+        else:
+            offer_name = f'{offer_name[:max_name_length-3]}...'
+
+    # Add offer name to IG story image
+    im_story = add_text_to_image(
+        im=im_story,
+        font_size=40,
+        x=27.5, # (10+17.5)
+        y=827.5, # (100+700+20+7.5)
+        text= '\n'.join(wrap(text=offer_name, width=30)),
+        text_align='left'
+    )
+    
+    # Prepare offer price text
+    offer_price_to = f'{offer_price_to:.2f}'.replace('.',',')
+    if offer_price_from == None:
+        price_text = f'Por apenas R${offer_price_to}'
+    else:
+        offer_price_from = f'{offer_price_from:.2f}'.replace('.',',')
+        price_text = f'De R${offer_price_from} por apenas R${offer_price_to}'
+    
+    # Add offer price text to IG story image
+    im_story = add_text_to_image(
+        im=im_story,
+        font_size=37.5,
+        x=27.5, # (10+17.5)
+        y=982.5, # (100+700+15+140+15+12.5)
+        text=price_text,
+        text_align='left'
+    )
+    
+    # Save IG story image as PNG file
+    im_story.save(fp=output_file)
+    
+    # Return output path
+    return output_file
