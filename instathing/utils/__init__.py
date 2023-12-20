afc803c08a8f48960485b53263cfd7b2283f863f
Daniel Rocha <danielsantos852@gmail.com>
2023-12-20 19:28:56 -0300

    Update __init__.py

--- instathing/utils/__init__.py
+++ instathing/utils/__init__.py
@@ -2,5 +2,6 @@ from .android import *
 from .execution import  *
 from .exporting import *
 from .image_manipulation import *
+from .image_search import *
 from .importing import *
 from .social_soul import *
