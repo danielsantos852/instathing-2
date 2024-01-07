5730c10970e73925844e2b4e16b2fde87d70c58b
Daniel Rocha <danielsantos852@gmail.com>
2024-01-07 19:44:29 -0300

    Delete __init__.py

--- sample/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .android import *
-from .execution import  *
-from .exporting import *
-from .image_manipulation import *
-from .image_search import *
-from .importing import *
-from .social_soul import *
