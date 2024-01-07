de1b1324bef565cf54b92977e7659e507a6e8ef7
Daniel Rocha <danielsantos852@gmail.com>
2024-01-07 17:14:06 -0300

    Update social_soul_load_android.py
    Minor improvements.

--- instathing/social_soul_load_android.py
+++ instathing/social_soul_load_android.py
@@ -1,22 +1,31 @@
 # Imports
 import config as cfg
-from utils import df_from_parquet, get_available_devices, connect_to_device, post_ig_story
+from utils import (df_from_parquet, 
+                   get_available_devices, 
+                   connect_to_device,
+                   launch_ig_app, 
+                   post_ig_story,
+                   time_sleep)
 
 
 # Global variables
-DF_SRC = (f'{cfg.DEFAULT_PATH_TO_TMP_PARQUET_FOLDER}'
-          f'{cfg.DEFAULT_IG_READY_PARQUET_FILE_NAME}.parquet')
-LINK_STICKER_TEXT = cfg.DEFAULT_LINK_STICKER_TXT
+INPUT_PARQUET = (f'{cfg.DEFAULT_TEMP_PARQUET_FOLDER}'
+                 f'{cfg.DEFAULT_LOAD_READY_PARQUET_FILE_NAME}.parquet')
+LINK_STICKER_TEXT = cfg.DEFAULT_LINK_STICKER_TEXT
 
 
 # Main Function
 def main():
     
     # Load offers dataframe
-    df_offers = df_from_parquet(input_parquet=DF_SRC)
+    df_offers = df_from_parquet(input_parquet=INPUT_PARQUET)
 
     # Connect to first available android device
-    device = connect_to_device(get_available_devices()[0].serial)
+    device = connect_to_device(device_serial=get_available_devices()[0].serial)
+
+    # Restart IG app on device
+    # launch_ig_app(device=device,
+    #               force_stop=True)
 
     # For each offer in offers dataframe:
     for i in range(len(df_offers)):
@@ -30,6 +39,9 @@ def main():
             close_friends=True,
             restart_ig_app=True,
         )
+
+        # Wait a little
+        time_sleep(5.0, 0.0)
 
 
 # If this is the script being run, call main()
