649c70d25a46c22d85488bfec5b25a6a7efc5142
Daniel Rocha <danielsantos852@gmail.com>
2024-01-09 14:29:59 -0300

    Update exec.py
    Removed:
    - time_sleep_var()
    
    Added:
    - time_sleep_randomized()

--- sample/helpers/exec.py
+++ sample/helpers/exec.py
@@ -3,37 +3,34 @@ from random import uniform
 from time import sleep
 
 
-def time_sleep_var(
-    t:float = 0.000,
-    delay:float = 000.00
+def time_sleep_randomized(
+    t_min:float = 0.0,
+    t_max:float = 0.0
 ) -> float:
     """
-    Adds a randomly-delayed time.sleep() call to program execution
+    Adds a randomized delay to programm execution
+
+    Calls time.sleep(t), where t is a random value in the range [t_min, t_max]
     
-    Calls time.sleep(x), where x is a random value in the range [t, t+t_delay]
-    
-    :param t: Base time (in seconds)
-    :type t: float
-    :param delay: Delay (0-100% of t)
-    :type delay: float
-    :return x: Calculated time
+    :param t_min: Minimum delay time (in seconds)
+    :type t_min: float
+    :param t_max: Maximum delay time (in seconds)
+    :type t_max: float
+
+    :return: Drawn number
     :rtype: float
     """
-    # Do not allow negative delay
-    if delay<0: 
-        delay = 0
-    # Do not allow delay over 100%
-    elif delay>100:
-        delay = 100
+    # Do not allow negative times
+    if t_min < 0.0:
+        t_min = 0.0
+    if t_max < 0.0:
+        t_max = 0.0
 
-    # Calculate delay in seconds
-    t_delay = t * (delay / 100)
+    # Calculate t
+    t = round(uniform(t_min, t_max), 4)
 
-    # Calculate x
-    x = t + (t_delay * round(uniform(0, 1), 3))
+    # Call time.sleep(t)
+    sleep(t)
 
-    # Call time.sleep(x)
-    sleep(x)
-
-    # Return x
-    return x
+    # Return t
+    return t
