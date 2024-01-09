7fb60f9a16a163f95dfa9d3d537909afbf0ddade
Daniel Rocha <danielsantos852@gmail.com>
2024-01-09 17:58:52 -0300

    Update context.py

--- sample/core/context.py
+++ sample/core/context.py
@@ -11,4 +11,4 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 # Internal imports
 import docs.config as config
-from helpers import *
+import sample.helpers as helpers
