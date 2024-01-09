476794085340aab67e53701b4d09f008c129c3a8
Daniel Rocha <danielsantos852@gmail.com>
2024-01-09 14:49:38 -0300

    Create context.py

---
+++ sample/context.py
@@ -0,0 +1,13 @@
+# Context file for "sample" folder
+
+# Python imports
+import os
+import sys
+
+
+# Set path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+# Internal imports
+import docs.config as config
