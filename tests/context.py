5acd0fb580e3fef206bbd70ff13072b8064059d6
Daniel Rocha <danielsantos852@gmail.com>
2024-01-09 14:49:34 -0300

    Update context.py

--- tests/context.py
+++ tests/context.py
@@ -1,5 +1,14 @@
+# Context file for "tests" folder
+
+# Python imports
 import os
 import sys
+
+
+# Set path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+
+# Internal imports
+import docs.config as config
 import sample
