46cf5d0ad244bd84d2515999e56dabc4dfbc95fd
Daniel Rocha <danielsantos852@gmail.com>
2024-01-09 14:36:08 -0300

    Renamed image_manipulation.py as image.py

--- sample/utils/image.py
+++ sample/utils/image.py
@@ -2,11 +2,60 @@
 from PIL import ImageDraw, ImageFont
 from PIL.PngImagePlugin import PngImageFile
 
+import pyautogui
+from pyscreeze import Box
+
 import config as cfg
 
 
 # Global Variables
 FONT_FILE_PATH = cfg.DEFAULT_FONT
+
+
+# Find in Image function
+def find_in_img(
+    needle_img:str|None = None,
+    haystack_img:str|None = None,
+    sureness:float = 0.9,
+) -> Box|None:
+    """
+    Finds a smaller, "needle" image inside a bigger, "haystack" image.
+    Returns coordinates and size of needle image occurrence in haystack image;
+    or None, if no match.
+    
+    Parameters:
+    - needle_img (str): image to search for;
+    - haystack_img (str): image where to look for;
+    - sureness (float): match sureness percentage (1.0 means "100% sure").
+
+    Returns
+    - box (Box): a pyscreeze Box object; or
+    - None: if needle image not found in haystack image.
+    """
+    
+    # force pyautogui's use of ImageNotFoundException
+    pyautogui.useImageNotFoundException()
+
+    # Try this:
+    try:
+    
+        # Locate subset image coordinates and size in image
+        box = pyautogui.locate(needleImage=needle_img,
+                               haystackImage=haystack_img,
+                               confidence=sureness)
+
+    # If image not found, print "I can't see" message
+    except pyautogui.ImageNotFoundException:
+        print('Needle image not found.')
+
+        # Return None
+        return None
+
+    # Print "image found" message
+    print(f'Needle image found at {box}')
+
+    # Return image box
+    return box
 
 
 # Add Text To Image function
